08f1712eefe041b50e0643a2b75e553d518e66fb
CBear <chebert222@gmail.com>
2021-05-12 13:11:28 -0700

    write file function added

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generate = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const generateQuestions = () => {
@@ -8,7 +9,7 @@ const generateQuestions = () => {
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username (Required)',
+            message: 'Enter your GitHub Username',
             validate: githubInput => {
                 if (githubInput) {
                     return true;
@@ -21,7 +22,7 @@ const generateQuestions = () => {
         {
             type: 'input',
             name: 'email',
-            message: 'Enter your Email (Required)',
+            message: 'Enter your Email',
             validate: emailInput => {
                 if (emailInput) {
                     return true;
@@ -30,13 +31,93 @@ const generateQuestions = () => {
                     return false;
                 }
             }
-        }
+        },
+        {
+            type: 'input',
+            name: 'project',
+            message: 'Enter the name of your project',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description of your project',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license you would like to use',
+            choices: ['Unlicense', 'WTFPL', 'MIT', 'Boost', 'Apache', 'Eclipse', 'None']
+        },
+        {
+             type: 'input',
+            name: 'install',
+            message: 'Enter a description of what to install in your project.',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter who contributed to the project and if anyone else can contribute.',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter people who contributed!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter a description of how to test your project',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter how to test your project!');
+                    return false;
+                }
+            }
+        },
     ])
+    .then(questionData => {
+        const { github, email} = questionData;
+        writeToFile();
+    })
 };
+
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {
+fs.writeFile('./utils/README.md', data, err => {
+    if (err) throw err;
 
+    console.log('README.md generated check out ')
+});
 }
 
 // TODO: Create a function to initialize app
@@ -47,4 +128,5 @@ const init = () => {
 // Function call to initialize app
  // init();
 
- generateQuestions();
+init()
+.then()
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,16 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 const renderLicenseBadge = (license) => {
+  if (!license) {
+    return '';
+  }
+ 
 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-const renderLicenseLink = (license) => {}
+const renderLicenseLink = (license) => {
+  if (!license) {
+    return '';
+  }
+  return `
+  ${license}
+  `
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-const renderLicenseSection = (license) => {}
+const renderLicenseSection = (license) => {
+  if (!license) {
+    return '';
+  }
+  return `
+  ${license}
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
